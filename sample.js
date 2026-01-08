f25ba883c199991dce8afc7cc309f452870e6c34
Arunilakkiyan <arunilakkiyan@gmail.com>
2026-01-08 13:16:26 +0530

    New Update

---
+++ sample.js
@@ -0,0 +1,14 @@
+function method1(){
+    console.log("method1 1");
+    
+}
+
+function method2(){
+    console.log("method2");
+    
+}
+function method3(){
+    console.log("method3");
+    
+}
+
--- tests/day1.spec.js
+++ tests/day1.spec.js
@@ -5,10 +5,11 @@ test("test", async ({ page }) => {
 
   await page.locator("#confirm").click();
   
+
   await page.waitForTimeout(3000)
  
-   page.on('dialog', dialog => dialog.accept());
-
+   page.on('dialog', dialog => dialog.accept())
+   ;
 
   await page.waitForTimeout(5000)
 
@@ -17,3 +18,11 @@ test("test", async ({ page }) => {
  
   
 });
+test("method 4",()=>{
+console.log("Method 4");
+
+})
+test("method 5",()=>{
+console.log("Method 5");
+
+})
